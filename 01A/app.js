0478f465139e9d1295dc7777c5143afe9b947707
troyma2007 <146706129+troyma2007@users.noreply.github.com>
2023-12-07 14:19:17 +0100

    update copyright

--- 01A/app.js
+++ 01A/app.js
@@ -12,9 +12,16 @@ app.runapplication();
 
 
 let appnaam = "demoapp"
+console.log("appnaam")
 let versienummer = 0.9;
+console.log("versienummer")
 let versiedatum = 2023;
+console.log("versiedatum")
 let autheur = "Troy"
+console.log("autheur")
 let copyright = "Troy bv"
+console.log("copyright")
 let distributeur = "Troy soft"
+console.log("distributeur")
 let darkmode = true 
+console.log("darkmode")

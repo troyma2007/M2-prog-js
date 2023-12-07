d2ebd8fb8048d55406eabd4afaad8b1f9643e400
troyma2007 <146706129+troyma2007@users.noreply.github.com>
2023-12-07 14:16:52 +0100

    copyright

--- 01A/app.js
+++ 01A/app.js
@@ -9,3 +9,12 @@ class app
 
 let app = new app();
 app.runapplication();
+
+
+let appnaam = "demoapp"
+let versienummer = 0.9;
+let versiedatum = 2023;
+let autheur = "Troy"
+let copyright = "Troy bv"
+let distributeur = "Troy soft"
+let darkmode = true 

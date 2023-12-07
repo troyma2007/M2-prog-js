447df144dc1ca021ae5d8fbbc45319309bacd2c6
troyma2007 <146706129+troyma2007@users.noreply.github.com>
2023-12-07 15:17:48 +0100

    update

---
+++ 01B/app.js
@@ -0,0 +1,37 @@
+class app
+{
+    runapplication()
+    {
+        console.log("Hello World!"); 
+    }
+}
+
+
+let app = new app();
+app.runapplication();
+
+console.log(app.greeting);
+console.log("appnaam:" + app.appnaam);
+console.log("versienummer:" + app.versienummer)
+console.log("versiedatum:" + app.versiedatum)
+console.log("autheur:" + app.autheur)
+console.log("copyright:" + app.copyright)
+console.log("distributeur:" + app.distributeur)
+console.logg("darkmode:" + app.darkmode)
+
+      
+let appnaam = "demoapp"
+console.log("appnaam")
+let versienummer = 0.9;
+console.log("versienummer")
+let versiedatum = 2023;
+console.log("versiedatum")
+let autheur = "Troy"
+console.log("autheur")
+let copyright = "Troy bv"
+console.log("copyright")
+let distributeur = "Troy soft"
+console.log("distributeur")
+let darkmode = true 
+console.log("darkmode")
+
